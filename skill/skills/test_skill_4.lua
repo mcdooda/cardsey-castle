dfd30b4b7bcb035ba78aa1d674e6d1258b347d47
mcdooda <nicolas.dod@gmail.com>
2019-09-22 16:31:18 +0200

    Fixed test skill 4

--- skill/skills/test_skill_4.lua
+++ skill/skills/test_skill_4.lua
@@ -6,7 +6,7 @@ skill.description = "Test skill 4"
 skill.requirements = "Numbers only"
 
 function skill:can_play_card(card_name, showing_front)
-	return (cards.is_joker(card_name) or cards.is_ace(card_name)) and showing_front
+	return (cards.is_joker(card_name) or cards.is_number(card_name)) and showing_front
 end
 
 function skill:execute(skill_script_component, card_name, showing_front)
