a0dfa59372d646719ac1869dae23589d87191ba2
mcdooda <nicolas.dod@gmail.com>
2019-09-30 23:46:12 +0200

    Added health point text, destroy card game object after a skill is used

--- skill/skills/test_skill_1.lua
+++ skill/skills/test_skill_1.lua
@@ -10,11 +10,11 @@ function skill:can_play_card(card_name)
 end
 
 function skill:execute(skill_script_component, card_name, showing_front, card_player_url, opponent_url)
-	local card_player_max_health_points = go.get(card_player_url, "health_points")
+	local card_player_max_health_points = go.get(card_player_url, "max_health_points")
 	local card_player_health_points = math.ceil(card_player_max_health_points / 2)
 	msg.post(card_player_url, "set_health_points", { health_points = card_player_health_points})
 	
-	local opponent_max_health_points = go.get(opponent_url, "health_points")
+	local opponent_max_health_points = go.get(opponent_url, "max_health_points")
 	local opponent_health_points = math.ceil(opponent_max_health_points / 2)
 	msg.post(opponent_url, "set_health_points", { health_points = opponent_health_points})
 end
