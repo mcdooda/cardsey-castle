b0705193e402e99ba7e012a1a02292488f31bab1
mcdooda <nicolas.dod@gmail.com>
2019-10-05 16:06:29 +0200

    Added skill spawning

--- character/character.go
+++ character/character.go
@@ -13,3 +13,21 @@ components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "skill-collection-factory"
+  type: "collectionfactory"
+  data: "prototype: \"/skill/skill-collection.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
